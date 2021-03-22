763d0f52cfac435099914c5ea566088e1c17fe5f
Lin Duan <ld131997@126.com>
2021-03-21 21:38:16 -0400

    Java输入输出格式

---
+++ 算法模板代码/acw_Java.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static int[] q;
+    
+    public static void main(String[] args) throws IOException{
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String[] ip1 = bf.readLine().split(" ");
+        int N = Integer.parseInt(ip1[0]);
+        String[] ip2 = bf.readLine().split(" ");
+    
+    }
+}
